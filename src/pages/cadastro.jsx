3ad9538126161ee5b9726a4b038ec07880422226
picolloo <lucaspicolloo@gmail.com>
2021-09-21 23:29:29 -0300

    redirect after create animal

--- src/pages/cadastro.jsx
+++ src/pages/cadastro.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 import axios from 'axios'
+import { useHistory} from 'react-router-dom'
+
 
 import { Navbar, Footer } from '../components'
 
@@ -9,6 +11,7 @@ export default function Cadastro() {
   const [status, statusSet] = useState("")
   const [location, locationSet] = useState("")
   const [foundAt, foundAtSet] = useState(new Date())
+  const [loading, loadingSet] = useState(false)
   const [errors, errorsSet] = useState({
     name: false,
     species: false,
@@ -16,34 +19,37 @@ export default function Cadastro() {
     location: false,
     foundAt: false
   })
+  const history = useHistory();
 
   function validateForm() {
     errorsSet({
-      name: !!name,
-      species: !!species,
-      status: !!status,
-      location: !!location,
-      foundAt: !!foundAt
+      name: !name,
+      species: !species,
+      status: !status,
+      location: !location,
+      foundAt: !foundAt
     })
 
-    return Object.values(errors).every(e => e)
+    return Object.values(errors).some(e => !e)
   }
 
   async function handleSubmit(e) {
     e.preventDefault()
 
     if (!validateForm()) return
+    loadingSet(true)
 
     await axios.post(
       process.env.REACT_APP_SERVER_URL,
       {name,species,status,location,foundAt}
     )
+    return history.push("/animals")
   }
 
   return (
     <main>
       <Navbar />
-      <div className="my-10">
+      <div className="my-14">
         <form className="flex flex-col items-center w-96 mx-auto gap-4">
           <div className="flex flex-col w-full gap-2">
             <label className="block text-gray-700 text-sm font-bold mb-2"
@@ -57,6 +63,7 @@ export default function Cadastro() {
               value={name}
               onChange={e => nameSet(e.target.value)}
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"/>
+        {errors.name && <p className="text-red-500 text-xs italic">Nome inválido.</p>}
           </div>
           <div className="flex flex-col w-full gap-2">
             <label className="block text-gray-700 text-sm font-bold mb-2"
@@ -73,13 +80,14 @@ export default function Cadastro() {
               <option value="gato">Gato</option>
               <option value="cachorro">Cachorro</option>
             </select>
+        {errors.species && <p className="text-red-500 text-xs italic">Espécie inválida.</p>}
           </div>
           <div className="flex flex-col w-full gap-2">
             <label className="block text-gray-700 text-sm font-bold mb-2"
               htmlFor="status">
               Status
             </label>
-            <select name="status" required
+            <select name="status"
               className="block w-full bg-white border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
               value={status}
               onChange={e => statusSet(e.target.value)}
@@ -89,6 +97,7 @@ export default function Cadastro() {
               <option value="resgatado">Resgatado</option>
               <option value="adotado">Adotado</option>
             </select>
+        {errors.status && <p className="text-red-500 text-xs italic">Status inválido.</p>}
           </div>
           <div className="flex flex-col w-full gap-2">
             <label className="block text-gray-700 text-sm font-bold mb-2"
@@ -102,6 +111,7 @@ export default function Cadastro() {
               onChange={e => locationSet(e.target.value)}
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             />
+    {errors.location && <p className="text-red-500 text-xs italic">Localização inválida.</p>}
           </div>
           <div className="flex flex-col w-full gap-2">
             <label className="block text-gray-700 text-sm font-bold mb-2"
@@ -115,12 +125,14 @@ export default function Cadastro() {
               onChange={e => foundAtSet(e.target.value)}
               className="block w-full bg-white border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
             />
+    {errors.foundAt && <p className="text-red-500 text-xs italic">Data inválida.</p>}
           </div>
           <div>
             <button
               className="font-bold curor-pointer bg-yellow-500 text-white py-2 px-4 hover:bg-yellow-400 rounded focus:outline-none focus:shadow-outline"
               type="submit"
               onClick={e => handleSubmit(e)}
+              disabled={loading}
             >
               Enviar
             </button>
